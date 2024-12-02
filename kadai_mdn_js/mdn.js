8f71159c0c7515db81a4ddc3098cb1f65e83fe3f
mi <nya7_mi@icloud.com>
2024-12-02 11:44:59 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+// 今日の日付を取得
+const today = new Date();
+
+// 年、月、日を取得
+const year = today.getFullYear();
+const month = today.getMonth() +1;
+const date = today.getDate();
+
+// 日付を出力
+console.log(year + '年' + month + '月' + date + '日')
